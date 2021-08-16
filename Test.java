f264b3f98b4aa49dcd4271d25e9d9f9f2dd3121c
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-08-16 21:43:31 +0530

    Intial Code commit for Batch 22

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Kokila, Welcome to java first code");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
+
+class Test1 {
+
+}
+
+class Test2 {
+
+}
+
+class Test3 {
+
+}
